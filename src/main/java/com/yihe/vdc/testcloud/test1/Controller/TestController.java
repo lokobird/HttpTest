b9c6e76f36ec8b3247f967588a34725d3636fd14
zhengwenzhe <1726474652@qq.com>
2016-10-31 10:10:16 +0800

    Initial commit

---
+++ src/main/java/com/yihe/vdc/testcloud/test1/Controller/TestController.java
@@ -0,0 +1,28 @@
+package com.yihe.vdc.testcloud.test1.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by zwz
+ * Date: 2016/8/4 14:13.
+ */
+@Controller
+public class TestController {
+
+    //private Logger log= LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/test")
+    public void test(HttpServletResponse response){
+        try {
+            response.getWriter().write("test123");
+            response.getWriter().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
